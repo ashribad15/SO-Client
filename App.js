a32e3bd9fb31421f958e4abb20a1c21b0ffa7990
ASHRIBAD PANDA <133385084+ashribad15@users.noreply.github.com>
2024-05-29 22:35:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/navbar/navbar.jsx';
+import AllRoutes from './AllRoutes.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <AllRoutes />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
